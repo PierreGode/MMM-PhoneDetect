8d5f36af1d1bd07d3346aedee00e31430adb7bb1
PierreGode <8579922+PierreGode@users.noreply.github.com>
2024-01-03 09:46:57 +0100

    Update MMM-PhoneDetect.js

--- MMM-PhoneDetect.js
+++ MMM-PhoneDetect.js
@@ -1,7 +1,7 @@
-Module.register("MMM-PhoneDetect", {
+Module.register('MMM-PhoneDetect', {
   // Default module config
   defaults: {
-    phones: [], // List of phone MAC addresses to detect, to be defined in config.js
+    phones: '', // List of phone MAC addresses to detect, to be defined in config.js
     turnOnCommand: "vcgencmd display_power 1 2", // Command to turn on the mirror
     turnOffCommand: "vcgencmd display_power 0 2", // Command to turn off the mirror
     checkInterval: 5000, // Check for phone presence every 5 seconds
@@ -9,7 +9,7 @@ Module.register("MMM-PhoneDetect", {
 
   // Start method
   start: function () {
-    Log.info("Starting MMM-PhoneDetect module");
+    console.log("Starting MMM-PhoneDetect module");
     this.sendSocketNotification("CONFIG", this.config);
   },
 
