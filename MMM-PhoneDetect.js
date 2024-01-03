c0278aa253e4c99584b5eb4cc9b2aceb5df4969c
PierreGode <8579922+PierreGode@users.noreply.github.com>
2024-01-03 13:56:29 +0100

    Update MMM-PhoneDetect.js

--- MMM-PhoneDetect.js
+++ MMM-PhoneDetect.js
@@ -1,18 +1,41 @@
 Module.register('MMM-PhoneDetect', {
   // Default module config
   defaults: {
-    phones: [], // List of phone MAC addresses to detect, to be defined in config.js
+    phones: [], // List of phone MAC addresses to detect
     checkInterval: 5000, // Check for phone presence every 5 seconds
   },
 
-  // Start method
   start: function () {
     console.log("Starting MMM-PhoneDetect module");
     this.sendSocketNotification("CONFIG", this.config);
+    this.onlineDevices = new Set(); // Track online devices
   },
 
-  // Notification handler
-  notificationReceived: function (notification, payload, sender) {
-    // Handle any specific notifications if needed
+  getDom: function() {
+    var wrapper = document.createElement("div");
+
+    this.config.phones.forEach(phone => {
+      var dot = document.createElement("span");
+      dot.className = "phone-dot " + (this.onlineDevices.has(phone) ? "online" : "offline");
+      wrapper.appendChild(dot);
+    });
+
+    return wrapper;
+  },
+
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "PHONE_PRESENCE") {
+      // Update onlineDevices based on payload
+      // Assuming payload contains the status of each phone
+      payload.forEach(phone => {
+        if (phone.isOnline) {
+          this.onlineDevices.add(phone.mac);
+        } else {
+          this.onlineDevices.delete(phone.mac);
+        }
+      });
+
+      this.updateDom();
+    }
   },
 });
